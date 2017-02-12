d4de6da7b651a6d2fe370582ae3e8779f1f157d8
shou <heyheymymy194@gmail.com>
2017-02-12 21:49:13 +0900

    edit config production.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+
   devise_for :users
 
   resources :users, only: [:index, :show, :edit, :update] do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110140414) do
+ActiveRecord::Schema.define(version: 20170212073002) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,6 +31,13 @@ ActiveRecord::Schema.define(version: 20170110140414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
